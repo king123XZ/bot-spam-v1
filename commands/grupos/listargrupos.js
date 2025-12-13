2a614fb0e749dcb00ed7dc954f5ef3d57c24047b
King123XZ <yersonorosco2002@gmail.com>
2025-12-13 15:08:51 -0500

    Update listargrupos.js

--- commands/grupos/listargrupos.js
+++ commands/grupos/listargrupos.js
@@ -1,15 +1,16 @@
 module.exports = {
   command: ["listargrupos", "grupos"],
-  description: "Lista todos los grupos guardados.",
   isOwner: true,
 
   run: async (client, m) => {
     if (!global.gruposAuto.length)
-      return m.reply("⚠️ No hay grupos guardados todavía.");
+      return m.reply("⚠️ No hay grupos guardados.");
 
     let texto = "📌 *Grupos guardados:*\n\n";
+
     global.gruposAuto.forEach((g, i) => {
-      texto += `${i + 1} ➤ ${g}\n`;
+      texto += `${i + 1}. 🧩 ${g.nombre}\n`;
+      texto += `   └─ ${g.jid}\n\n`;
     });
 
     m.reply(texto);
